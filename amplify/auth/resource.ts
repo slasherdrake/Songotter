a0e6c4c6bbf8448f93f87ff81212c2237c52a3cc
slasherdrake <dkdrakekendall@gmail.com>
2024-10-06 20:55:51 -0700

    take 5

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,13 +7,11 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
-
   },
-    
-  userAttributes: {
-    preferredUsername: {
-      required: true,
-      
-    }
-  }
+  // userAttributes: {
+  //   preferredUsername: {
+  //     mutable: true,
+  //     required: true
+  //   }
+  // }
 });
